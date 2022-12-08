ba27ce493dce7ba07e8e8fe1fdcd8ea763187c41
ted182 <tedesco182@gmail.com>
2022-12-08 15:48:57 -0300

    Update arrumaBag.js

--- arrumaBag.js
+++ arrumaBag.js
@@ -1,4 +1,4 @@
-function arrumapots()
+function arrumaPots()
 {
 	Orion.Print('Selecione a BAG da arrumacao');
 	Orion.AddObject('BAG')
@@ -6,21 +6,21 @@ function arrumapots()
 	{
 		Orion.Wait(10);
 	}
-	var potionLIST = 
+	
+	const potionList = 
 	[
-		['0x0F82','0x0026'],	//life boost
-		['0x0F82','0x0532'],	//mana boost
-		['0x0F82','0x017F'],	//reffiling
-		['0x0F0E','0x0001'],	//invis
-		['0x0F0E','0x0022'],	//refresh
-		['0x0F0E','0x008F'],	//cure 
-		['0x0F0E','0x0099'],	//greater heal
-		['0x0F0E','0x0480'], 	//total mana    	
-		['0x1F5F','0x0000'], 	//FS
-		['0x0E21','0x0000'] 	//bands 
+		{ type:'0x0F82', color:'0x0026' },	//life boost
+		{ type:'0x0F82', color:'0x0532' },	//mana boost
+		{ type:'0x0F82', color:'0x017F' },	//reffiling
+		{ type:'0x0F0E', color:'0x0001' },	//invis
+		{ type:'0x0F0E', color:'0x0022' },	//refresh
+		{ type:'0x0F0E', color:'0x008F' },	//cure 
+		{ type:'0x0F0E', color:'0x0099' },	//greater heal
+		{ type:'0x0F0E', color:'0x0480' }	//total mana
+		
     	];
     	
- 	var reaglist = 
+ 	const reagList = 
 	[
 		"0x0F84", //garlic
 		"0x0F7B", //bloodmoss
@@ -29,52 +29,60 @@ function arrumapots()
 		"0x0F86", //mandrake roots
 		"0x0F88", //night shade
 		"0x0F8C", //sulfurus ash
-		"0x0F8D"  //spider silk
-	]; 
- 	
- 	var x1 = 45;
+		"0x0F8D",  //spider silk
+		"0x1F5F",	//FS
+		"0x0E21"	//bands
+	];
+	
+	//posições das potions (variáveis)
+	var x1 = 45;
 	var y1 = 127;
 	var x2 = 142;
 	var y2 = 126;
-
- 	for (i = 0; i < potionLIST.length; i++)
+	
+	const pegaElarga = function(x,y,obj)
 	{
-		list = Orion.FindType(potionLIST[i][0] , potionLIST[i][1], 'BAG');
-		if (list.length > 0)
+		if ( obj )
 		{
-			Orion.DragItem(list[0]);
-			Orion.DropDraggedItem('BAG', x1, y1, 0);
-			Orion.Wait('moveitemdelay');
-			x1 = x1 + 10;
-		};			
+			if ( obj.Count() > 0 && (obj.X() != x || obj.Y() != y) )
+			{
+				Orion.DragItem( obj.Serial() );
+				Orion.DropDraggedItem('BAG', x, y, 0);
+				Orion.Wait('moveitemdelay');
+			}
+		}
 	};
 	
-	for (i = 0; i < reaglist.length; i++)
-	{
-		while (true)
-		{
-			list = Orion.FindType(reaglist[i] , '-1', 'BAG');
-			if (list[0] == null)
-				break
-			if (Orion.FindObject(list[0]).X() == x2)
-				break
-			Orion.DragItem(list[0]);
-			Orion.DropDraggedItem('BAG', x2, y2, 0);
-			Orion.Wait('moveitemdelay');	
-		}
-	}
-
-	//stacka potions de explosion
+	potionList.forEach(function(el,index){  
+		
+		obj = Orion.FindObject( Orion.FindType(el.type, el.color, 'BAG')[0] );
+		pegaElarga( x1, y1, obj );
+		x1 = x1 + 10;
+	
+	});
+	
+	reagList.forEach(function(el,index){  
+	
+		obj = Orion.FindObject( Orion.FindType(el, -1, 'BAG')[0] );
+		pegaElarga( x2, y2, obj );
+	
+	});
+	
+	//stack explosion
 	var x3 = 142;
 	var y3 = 75;
-	var qntexplo = Orion.Count('0x0F02' , '0x0000', 'BAG');	
-	for (i = 0; i < qntexplo; i++) 
-	{
-		list = Orion.FindType('0x0F02' , '0x0000', 'BAG');
-		Orion.DragItem(list[0]);
-		Orion.DropDraggedItem('BAG', x3, y3);
-		Orion.Wait('moveitemdelay');
-	}
-	Orion.Print( 'FINALIZADO');	
+	explosions = Orion.FindType('0x0F02' , '0x0000', 'BAG');
 	
-};
+	if ( explosions )
+	{
+		explosions.forEach(function(el,index){  
+			
+			obj = Orion.FindObject( el );
+			pegaElarga( x3, y3, obj );
+		
+		});
+	}
+	
+	Orion.CharPrint(self, 0x0021, 'Arrumação finalizada!'); 
+
+}
